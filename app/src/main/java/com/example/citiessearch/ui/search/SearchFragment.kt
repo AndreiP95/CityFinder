da5576318d6f059f81070a42cd586d7bcdd36dfd
AndreiPancu <andreipancu95@gmail.com>
2020-06-16 20:03:51 +0300

    Created UI for search feature

---
+++ app/src/main/java/com/example/citiessearch/ui/search/SearchFragment.kt
@@ -0,0 +1,31 @@
+package com.example.citiessearch.ui.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.citiessearch.R
+import com.example.citiessearch.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_search, container, false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = this
+    }
+
+}
---
+++ app/src/main/java/com/example/citiessearch/ui/search/SearchViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.citiessearch.ui.search
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel()
